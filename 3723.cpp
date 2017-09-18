a0e15cd8ed6b44f757c453bb906e050965d4ee04
kn1cht <ito.turukasiwa@gmail.com>
2017-09-19 00:52:43 +0900

    3723

--- 3723.cpp
+++ 3723.cpp
@@ -17,7 +17,6 @@
 #include <string>
 #include <utility>
 #include <vector>
-//#include <bits/stdc++.h>
 #define FOR(i,k,n) for (int (i)=(k); (i)<(n); ++(i))
 #define RFOR(i,k,n) for (int (i)=(n)-1; (i)>=(k); --(i))
 #define REP(i,n) FOR(i,0,n)
@@ -40,43 +39,7 @@ typedef long long int LL;
 typedef unsigned long long int ULL;
 typedef vector<int> VI;
 typedef vector<VI> VVI;
-typedef vector<LL> VLL;
-typedef vector<VLL> VVLL;
-typedef vector<char> VC;
-typedef vector<VC> VVC;
-typedef vector<bool> VB;
-typedef vector<VB> VVB;
-typedef pair<int, int> PII;
 
-// initialization
-template<typename A, size_t N, typename T> void FILL(A (&array)[N], const T &val){
-  fill( (T*)array, (T*)(array+N), val );
-}
-
-// pair
-template<typename T1, typename T2> ostream& operator<<(ostream& s, const pair<T1, T2>& p) {
-  return s << "(" << p.first << ", " << p.second << ")";
-}
-// vector
-template<typename T> ostream& operator<<(ostream& s, const vector<T>& v) {
-  int len = v.size();
-  for (int i = 0; i < len; ++i) {
-    s << v[i]; if (i < len - 1) s << "\t";
-  }
-  return s;
-}
-// 2 dimentional vector
-template<typename T> ostream& operator<<(ostream& s, const vector< vector<T> >& vv) {
-  int len = vv.size();
-  for (int i = 0; i < len; ++i) {
-    s << vv[i] << endl;
-  }
-  return s;
-}
-
-int main() {
-  return 0;
-}
 
 // Union-Find Tree
 struct Union {
@@ -99,29 +62,8 @@ struct edge { int u, v, c; };
 bool comp_e(const edge& e1, const edge& e2) { return e1.c < e2.c; }
 struct Graph {
   const int N;
-  vector<vector<PII> > data; // fst : to, snd : cost
   vector<edge> data_e;
-  Graph(int size) : N(size + 1), data(N) {}
-
-  VI dijkstra(int s) {
-    priority_queue<PII, vector<PII>, greater<PII> > PQ;
-    VI distance(N, 1e9);
-    distance[s] = 0;
-    PQ.push(mp(0, s));
-    while(!PQ.empty()) {
-      PII p = PQ.top(); PQ.pop();
-      int v = p.snd;
-      if(distance[v] < p.fst) { continue; }
-      for(const auto & e : data[v]) {
-        if(distance[e.fst] > distance[v] + e.snd) {
-          distance[e.fst] = distance[v] + e.snd;
-          PQ.push(mp(distance[e.fst], e.fst));
-        }
-      }
-    }
-    return distance;
-  }
-  /* ***use data_e for kruskal method***
+  Graph(int size) : N(size + 1), data_e(N) {}
 
   int kruskal(int V) {
     sort(all(data_e), comp_e);
@@ -136,12 +78,20 @@ struct Graph {
     }
     return res;
   }
-  */
 };
 
-// complex
-typedef complex<double> xy_t;
-double dot_product(xy_t a, xy_t b){ return (conj(a) * b).real(); }
-double cross_product(xy_t a, xy_t b){ return (conj(a) * b).imag(); }
-xy_t projection(xy_t p, xy_t b) {return b * dot_product(p,b) / norm(b);}
+int main() {
+  int T, N, M, R, x, y, d;
+  cin >> T;
+  REP(i, T) {
+    cin >> N >> M >> R;
+    Graph G(R);
+    REP(i, R) {
+      scanf("%d %d %d", &x, &y, &d);
+      G.data_e[i] = (edge){x, y + N, -d};
+    }
+    cout << 10000 * (N + M) + G.kruskal(N + M) << endl;
+  }
+  return 0;
+}
 
